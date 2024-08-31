e67f634de27d5afeeb08180188b2740f2581bd9e
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-08-31 19:44:34 +0200

    Refactor

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 from time import strptime
 import smtplib
 import ssl
+from typing import List
 
 months = {
     "Jan.": "01",
@@ -21,17 +22,18 @@ months = {
 
 today = date.today()
 expired_certs = []
+today_expiring_certs = []
 soon_expiring_certs = []
 message_content = ""
-DAYS = 7
+NEXT_DAYS = 7
 
 
-def send_email():
-    smtp_server = "mail.inbox.lv"
+def send_email() -> None:
+    smtp_server = "xxxxx"
     port = 465
-    sender_email = "wlucert556@mail.lv"
-    receiver_email = "wlucert556@mail.lv"
-    password = "VxKp4sBW2?"
+    sender_email = "xxxxx"
+    receiver_email = "xxxxx"
+    password = "xxxxx"
 
     message = EmailMessage()
     message["From"] = sender_email
@@ -47,64 +49,117 @@ def send_email():
         print("The e-mail has been sent.")
 
 
-with open("sample_data.csv") as file:
-    for row in file:
-        name: str = row.split(",")[0].split(";")[0]
-        day: str = row.split(";")[1].split(",")[0].split(" ")[1]
-        month: str = row.split(",")[0].split(";")[1].split(" ")[0]
-        month: str = months[month]
-        year: str = row.split(",")[1].strip()
-        date: str = year + "-" + month + "-" + day
-        date = strptime(date, "%Y-%m-%d")
-        date_of_expire: datetime.date = datetime(*date[:3]).date()
-        calculate_days_to_expire = date_of_expire - today
-        days_to_expire = str(calculate_days_to_expire).split(",")[0].split(" ")[0]
-        date = date_of_expire.strftime('%Y.%m.%d')
-        if days_to_expire == "0:00:00":
-            days_to_expire = 0
-
-        days_to_expire = int(days_to_expire)
-        if days_to_expire == 0:
-            cert = f"{name} - expires today"
-            soon_expiring_certs.append(cert)
-        elif 0 < days_to_expire <= 7:
-            cert = f"{name} - expires in {days_to_expire} days ({date})"
-            soon_expiring_certs.append(cert)
-        elif days_to_expire < 0:
-            cert = f"{name} - expired {-days_to_expire} days ago ({date})"
-            expired_certs.append(cert)
+def get_all_certs_from_file(filename: str) -> List[dict]:
+    certs = []
+    with open(filename) as file:
+        for row in file:
+            cert = parse_data(row)
+            certs.append(cert)
+    return certs
 
 
-def get_expired_certs_str(expired_certs: list) -> str:
+def get_date_of_expire(data: dict) -> datetime.date:
+    date: str = data["year"] + "-" + data["month"] + "-" + data["day"]
+    date = strptime(date, "%Y-%m-%d")
+    date_of_expire: datetime.date = datetime(*date[:3]).date()
+    return date_of_expire
+
+
+def calculate_days_to_expire(date_of_expire: datetime.date) -> int:
+    days_to_expire = date_of_expire - today  # if today, it returns no days..., only "0:00:00"
+    days_to_expire = str(days_to_expire).split(",")[0].split(" ")[0]  # ... so this slicing doesn't work...
+    if days_to_expire == "0:00:00":  # ... so we need to manually set the days
+        days_to_expire = 0
+    days_to_expire = int(days_to_expire)
+    return days_to_expire
+
+
+def create_expired_certs_str(expired_certs: list) -> str:
     expired_certs_str = ""
     for expired_cert in expired_certs:
         expired_certs_str += expired_cert + "\n"
     return expired_certs_str
 
 
-def get_expiring_soon_certs_str(expiring_certs: list) -> str:
-    soon_expiring_certs_str = ""
-    for soon_expiring_cert in expiring_certs:
-        soon_expiring_certs_str += soon_expiring_cert + "\n"
-    return soon_expiring_certs_str
+def parse_data(cert: str) -> dict:
+    name: str = cert.split(",")[0].split(";")[0]
+    day: str = cert.split(";")[1].split(",")[0].split(" ")[1]
+    month: str = cert.split(",")[0].split(";")[1].split(" ")[0]
+    month: str = months[month]
+    year: str = cert.split(",")[1].strip()
+    return {
+        "name": name,
+        "day": day,
+        "month": month,
+        "year": year
+    }
 
 
-if __name__ == '__main__':
-    if expired_certs or soon_expiring_certs:
-        if expired_certs:
-            expired_certs_str = get_expired_certs_str(expired_certs=expired_certs)
-            message_content += f"""
-        Certificates that have expired:
-        {expired_certs_str}
-        """
-            print(f"Certificates that have expired: \n{expired_certs_str}")
-        if soon_expiring_certs:
-            soon_expiring_certs_str = get_expiring_soon_certs_str(expiring_certs=soon_expiring_certs)
-            message_content += f"""
-            Certificates that will expire in the next {DAYS} days:
-        {soon_expiring_certs_str}
-        """
-            print(f"Certificates that will expire in the next {DAYS} days: \n{soon_expiring_certs_str}")
+def parse_file(filename) -> List[str]:
+    new_list_certs = []
+    with open(filename) as file:
+        for row in file:
+            new_list_certs.append(row)
+    return new_list_certs
+
+
+def is_certificate_expired(days_to_expire) -> bool:
+    if days_to_expire < 0:
+        return True
+    return False
+
+
+def is_certificate_expiring_soon(days_to_expire) -> bool:
+    if 0 < days_to_expire <= NEXT_DAYS:
+        return True
+    return False
+
+
+def is_certificate_expiring_today(days_to_expire) -> bool:
+    if days_to_expire == 0:
+        return True
+    return False
+
+
+def create_message_content(certs: list, message_prefix: str) -> str:
+    message_content = ""
+    message_content += message_prefix
+    for cert in certs:
+        message_content += cert + "\n"
+    return message_content
+
+
+if __name__ == "__main__":
+    certificates = parse_file("sample_data.csv")
+
+    for certificate in certificates:
+        data = parse_data(certificate)
+        date_of_expire = get_date_of_expire(data=data)
+        days_to_expire = calculate_days_to_expire(date_of_expire=date_of_expire)
+        date = date_of_expire.strftime("%Y.%m.%d")
+
+        if is_certificate_expiring_today(days_to_expire=days_to_expire):
+            cert = f"{data['name']} - expires today"
+            today_expiring_certs.append(cert)
+        elif is_certificate_expiring_soon(days_to_expire=days_to_expire):
+            cert = f"{data['name']} - expires in {days_to_expire} days ({date})"
+            soon_expiring_certs.append(cert)
+        elif is_certificate_expired(days_to_expire=days_to_expire):
+            cert = f"{data['name']} - expired {-days_to_expire} days ago ({date})"
+            expired_certs.append(cert)
+
+    if today_expiring_certs:
+        message_content += create_message_content(certs=today_expiring_certs,
+                                                  message_prefix="Certificates that expire today:\n") + "\n"
+    if expired_certs:
+        message_content += create_message_content(certs=expired_certs,
+                                                  message_prefix="Certificates that have expired:\n") + "\n"
+    if soon_expiring_certs:
+        message_content += create_message_content(certs=soon_expiring_certs,
+                                                  message_prefix=f"Certificates that will expire in the next {NEXT_DAYS} days:\n") + "\n"
+
+    if message_content:
+        print(message_content)
         send_email()
     else:
         print("All certificates are current.")
